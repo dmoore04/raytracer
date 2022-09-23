2a6a591ae66cc77d4da02214cdaca0059e6066db
dmoore04 <dmoore04@proton.me>
2022-09-23 19:24:39 +0100

    implement basic ppm image generation

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,27 @@
 
 int main()
 {
-    std::cout << "Raytracer in a weekend!";
+    int image_height = 256;
+    int image_width = 256;
+
+    std::cout << "P3\n"
+              << image_height << ' ' << image_width << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
+
+            const auto MAX_COLOR = 255.999;
+
+            int ir = static_cast<int>(MAX_COLOR * r);
+            int ig = static_cast<int>(MAX_COLOR * g);
+            int ib = static_cast<int>(MAX_COLOR * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
