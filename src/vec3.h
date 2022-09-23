7a693451381127cdf5b8899232e4ba92e1b07c4e
dmoore04 <dmoore04@proton.me>
2022-09-23 19:56:27 +0100

    implement some arithmetic operators

--- src/vec3.h
+++ src/vec3.h
@@ -17,6 +17,29 @@ public:
     double operator[](int i) const { return e[i]; }
     double &operator[](int i) const { return e[i]; }
 
+    vec3 operator-() const { return vec3(-e[0] - e[1] - e[2]); }
+
+    vec3 &operator+=(const vec3 &v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3 &operator*=(const double t)
+    {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3 &operator/=(const double t)
+    {
+        return *this *= 1 / t;
+    }
+
 public:
     double e[3];
 }
